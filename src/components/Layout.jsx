1468a3f974a7033d9c74f91aa7d7ba5d383f180f
lugrao <elugrao@gmail.com>
2024-04-22 16:52:19 -0300

    add sidebar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,10 @@
+import Sidebar from "./Sidebar";
+
 const Layout = () => {
   return (
     <div className="flex h-full w-full items-center justify-center p-2">
-      <div className="h-full w-full rounded-3xl bg-white p-0.5">
+      <div className="h-full w-full rounded-3xl bg-white p-0.5 flex">
+        <Sidebar />
         <div className="h-full w-full rounded-3xl bg-violet-50"></div>
       </div>
     </div>
