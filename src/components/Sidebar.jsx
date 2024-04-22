c511b70347344771f9121764d63a711a33b26446
lugrao <elugrao@gmail.com>
2024-04-22 17:23:39 -0300

    add logo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,11 @@
 const Sidebar = () => {
-  return <div className="h-full w-72 rounded-l-3xl bg-white"></div>;
+  return (
+    <div className="flex h-full w-72 justify-center rounded-l-3xl bg-white py-9">
+      <div className="mt-0 flex h-20 w-20 justify-center">
+        <img src="/capsule-corp.svg" alt="Capsule Corp. logo" />
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
