4d1e9d3dfff1d1edd248903e0903a11c61eb11ec
lugrao <elugrao@gmail.com>
2024-04-22 17:48:32 -0300

    type isActive prop

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -1,5 +1,11 @@
-const NavButton = () => {
-  return <div></div>;
+import PropTypes from "prop-types";
+
+const NavButton = ({ isActive }) => {
+  return isActive && <div></div>;
+};
+
+NavButton.propTypes = {
+  isActive: PropTypes.boolean.isRequired,
 };
 
 export default NavButton;
