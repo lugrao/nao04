b313144767ab5a376960d6d2a91d6581c7fb941a
lugrao <elugrao@gmail.com>
2024-04-22 15:47:48 -0300

    add basic layout

---
+++ src/components/Layout.jsx
@@ -0,0 +1,11 @@
+const Layout = () => {
+  return (
+    <div className="flex h-full w-full items-center justify-center p-2">
+      <div className="h-full w-full rounded-3xl bg-white p-0.5">
+        <div className="h-full w-full rounded-3xl bg-violet-50"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
