88b6f8244a1052f831c00867c1ef832b4e06a3d8
lugrao <elugrao@gmail.com>
2024-04-23 11:04:52 -0300

    add logo and text

--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -1,7 +1,16 @@
 import PropTypes from "prop-types";
 
 const NavButton = ({ isActive }) => {
-  return isActive && <div></div>;
+  return (
+    isActive && (
+      <button className="btn">
+        <div>
+          <img src="src/assets/dashboard.svg" alt="Dashboard icon" />
+        </div>
+        <div>Dashboard</div>
+      </button>
+    )
+  );
 };
 
 NavButton.propTypes = {
