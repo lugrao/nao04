e20798b672a2431d64f8970f34946426b0ba4d91
lugrao <elugrao@gmail.com>
2024-04-23 17:48:45 -0300

    add inactive button

--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -4,20 +4,30 @@ export interface NavButtonProps {
 }
 
 const NavButton = ({ isActive, label }: NavButtonProps) => {
-  return (
-    isActive && (
-      <button className="btn flex w-44 justify-between bg-purple-50 shadow-lg hover:border-neutral-200 hover:bg-neutral-200">
-        <div>
-          <img
-            width={25}
-            height={25}
-            src={`src/assets/${label.toLowerCase()}.svg`}
-            alt={`Ícono de ${label}`}
-          />
-        </div>
-        <div className="text-base font-medium text-indigo-900">{label}</div>
-      </button>
-    )
+  return isActive ? (
+    <button className="btn flex w-44 justify-between bg-purple-50 shadow-lg hover:border-neutral-200 hover:bg-neutral-200">
+      <div>
+        <img
+          width={25}
+          height={25}
+          src={`src/assets/${label.toLowerCase()}.svg`}
+          alt={`Ícono de ${label}`}
+        />
+      </div>
+      <div className="text-base font-medium text-indigo-900">{label}</div>
+    </button>
+  ) : (
+    <button className="btn flex w-44 justify-between border-white bg-white hover:border-neutral-200 hover:bg-neutral-200">
+      <div>
+        <img
+          width={20}
+          height={20}
+          src={`src/assets/${label.toLowerCase()}-inactive.svg`}
+          alt={`Ícono de ${label}`}
+        />
+      </div>
+      <div className="text-base font-medium text-indigo-300">{label}</div>
+    </button>
   );
 };
 
