54f57333c12e80b48b0ce648a8c69bf06f385afd
lugrao <elugrao@gmail.com>
2024-04-23 12:08:31 -0300

    format code

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import Sidebar from "./Sidebar.tsx";
 const Layout = () => {
   return (
     <div className="flex h-full w-full items-center justify-center p-2">
-      <div className="h-full w-full rounded-3xl bg-white p-0.5 flex">
+      <div className="flex h-full w-full rounded-3xl bg-white p-0.5">
         <Sidebar />
         <div className="h-full w-full rounded-3xl bg-violet-50"></div>
       </div>
