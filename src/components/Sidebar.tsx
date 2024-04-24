6fc6bb045e67f401ba167037ea3acd98d0a7832b
lugrao <elugrao@gmail.com>
2024-04-24 17:30:51 -0300

    center elements

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import CustomerService from "./CustomerService";
 
 const Sidebar = () => {
   return (
-    <div className="flex h-full w-72 flex-col justify-center rounded-l-3xl bg-white py-9">
+    <div className="flex h-full w-72 flex-col items-center rounded-l-3xl bg-white py-9">
       <div className="mt-0 flex h-20 w-20 justify-center">
         <img src="/capsule-corp.svg" alt="Capsule Corp. logo" />
       </div>
