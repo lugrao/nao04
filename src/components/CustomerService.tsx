fc014132219bc7ebbe5e10f30fb8b8ac0d20cfd5
lugrao <elugrao@gmail.com>
2024-04-24 16:18:41 -0300

    format code

--- src/components/CustomerService.tsx
+++ src/components/CustomerService.tsx
@@ -5,7 +5,7 @@ const CustomerService = () => {
         <h2 className="text-center text-base font-semibold">
           Atención al cliente
         </h2>
-        <p className="text-sm text-stone-500 pt-2 pb-3">
+        <p className="pb-3 pt-2 text-sm text-stone-500">
           Nuestro equipo está disponible las 24 horas para resolver cualquier
           duda que tengas.
         </p>
