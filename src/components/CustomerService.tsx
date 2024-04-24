ac8082799ab42b3d50d6ea1aa4bc4175c54ad9f8
lugrao <elugrao@gmail.com>
2024-04-24 15:23:38 -0300

    add basic customer service card

--- src/components/CustomerService.tsx
+++ src/components/CustomerService.tsx
@@ -1,5 +1,20 @@
 const CustomerService = () => {
-  return <div></div>;
+  return (
+    <div className="card bg-base-100 shadow-xl">
+      <div className="card-body">
+        <h2 className="card-title">Atención al cliente</h2>
+        <p>
+          Nuestro equipo está disponible las 24 horas para resolver cualquier
+          duda que tengas.
+        </p>
+        <div className="card-actions justify-center">
+          <button className="btn">Conéctate ya</button>
+        </div>
+      </div>
+      <a href="">Términos y condiciones</a>
+      <a href="">Política de privacidad</a>
+    </div>
+  );
 };
 
 export default CustomerService;
