a26852746876c50ac7b6ba15a6c8b0b57d69cf68
lugrao <elugrao@gmail.com>
2024-04-25 10:32:51 -0300

    refactor sidebar styles; add overflow to nav buttons and customer service card

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,22 +3,25 @@ import CustomerService from "./CustomerService";
 
 const Sidebar = () => {
   return (
-    <div className="flex h-full w-72 flex-col items-center rounded-l-3xl bg-white py-9">
+    <div className="flex w-72 flex-col items-center rounded-l-3xl bg-white py-9">
       <div className="mb-12 mt-0 flex h-20 w-20 justify-center">
         <img src="/capsule-corp.svg" alt="Capsule Corp. logo" />
       </div>
-      <div className="flex flex-col gap-6">
-        <div className="mb-4">
-          <NavButton isActive={true} label="Dashboard" />
+
+      <div className="flex flex-col items-center overflow-scroll">
+        <div className="flex flex-col gap-6">
+          <div className="mb-4">
+            <NavButton isActive={true} label="Dashboard" />
+          </div>
+          <NavButton isActive={false} label="Ventas" />
+          <NavButton isActive={false} label="Productos" />
+          <NavButton isActive={false} label="Recibos" />
+          <div className="mt-8">
+            <NavButton isActive={false} label="Cerrar sesión" />
+          </div>
         </div>
-        <NavButton isActive={false} label="Ventas" />
-        <NavButton isActive={false} label="Productos" />
-        <NavButton isActive={false} label="Recibos" />
-        <div className="mt-8">
-          <NavButton isActive={false} label="Cerrar sesión" />
-        </div>
+        <CustomerService />
       </div>
-      <CustomerService />
     </div>
   );
 };
