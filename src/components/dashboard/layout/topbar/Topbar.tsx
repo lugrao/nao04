0597b66ee4bf36cb630b4e8ee92af9b13de339be
lugrao <elugrao@gmail.com>
2024-04-30 14:48:29 -0300

    add TotalIncome component

--- src/components/dashboard/layout/topbar/Topbar.tsx
+++ src/components/dashboard/layout/topbar/Topbar.tsx
@@ -1,3 +1,9 @@
+import { TotalIncome } from "./TotalIncome";
+
 export const Topbar = () => {
-  return <div className="h-36 w-full bg-violet-50 "></div>;
+  return (
+    <div className="h-36 w-full bg-violet-50 ">
+      <TotalIncome />
+    </div>
+  );
 };
