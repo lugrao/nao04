bb44f84f37ee449efec2d83025810d1596957953
lugrao <elugrao@gmail.com>
2024-04-30 12:05:19 -0300

    import and render Topbar component

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -1,9 +1,11 @@
 import { Sidebar } from "../components/dashboard/layout/sidebar/Sidebar.tsx";
+import { Topbar } from "../components/dashboard/layout/topbar/Topbar.tsx";
 
 export const Dashboard = () => {
   return (
     <div className="flex">
       <Sidebar />
+      <Topbar />
     </div>
   );
 };
