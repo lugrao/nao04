a0a7a9f1befd1388b9d8ad25f3837a1a732eb09e
lugrao <elugrao@gmail.com>
2024-04-30 11:49:37 -0300

    rearrange sidebar component within dashboard layout

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -4,8 +4,9 @@ export const Dashboard = () => {
   return (
     <div className="flex h-full w-full items-center justify-center p-2">
       <div className="flex h-full w-full rounded-3xl bg-white p-0.5">
-        <Sidebar />
-        <div className="h-full w-full rounded-3xl bg-violet-50"></div>
+        <div className="flex h-full w-full rounded-3xl bg-violet-50">
+          <Sidebar />
+        </div>
       </div>
     </div>
   );
