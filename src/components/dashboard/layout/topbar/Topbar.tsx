f99ed6b316067339591931da84cf94ff692552ab
lugrao <elugrao@gmail.com>
2024-04-30 16:07:18 -0300

    add TotalIncome prop values

--- src/components/dashboard/layout/topbar/Topbar.tsx
+++ src/components/dashboard/layout/topbar/Topbar.tsx
@@ -3,7 +3,7 @@ import { TotalIncome } from "./TotalIncome";
 export const Topbar = () => {
   return (
     <div className="h-36 w-full bg-violet-50 ">
-      <TotalIncome />
+      <TotalIncome totalIncome={71813.26} expenses={1294} earnings={1294} />
     </div>
   );
 };
