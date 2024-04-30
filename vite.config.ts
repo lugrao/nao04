99d250e035db73ae0ffad0f1c25c42247e640e3b
lugrao <elugrao@gmail.com>
2024-04-30 17:20:12 -0300

    configure absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: resolve(__dirname, "src"),
+      routes: resolve(__dirname, "src/routes"),
+      components: resolve(__dirname, "src/components"),
+      utils: resolve(__dirname, "src/utils"),
+    },
+  },
 });
