b992301ef466047b1efb1d18ba439466279869b0
lugrao <elugrao@gmail.com>
2024-04-30 12:39:36 -0300

    make child elements of layout clip their borders

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -5,7 +5,7 @@ export const Root = () => {
   return (
     <div className="flex h-full w-full items-center justify-center p-2">
       <div className="flex h-full w-full rounded-3xl bg-white p-0.5">
-        <div className="flex h-full w-full rounded-3xl bg-violet-50">
+        <div className="flex h-full w-full overflow-hidden rounded-3xl bg-violet-50">
           <Dashboard />
         </div>
       </div>
