34eba5725e66a9e14abb7197208911f83108dbe7
lugrao <elugrao@gmail.com>
2024-04-30 15:46:48 -0300

    align icons and text to the center

--- src/components/dashboard/layout/topbar/TotalIncome.tsx
+++ src/components/dashboard/layout/topbar/TotalIncome.tsx
@@ -1,34 +1,38 @@
 export const TotalIncome = () => {
   return (
-    <div className="flex w-min gap-7">
+    <div className="flex w-min gap-7 ">
       <div>
-        <h2 className="text-start text-sm">Ingreso total</h2>
-        <p className="text-2xl font-semibold">$71,813.26</p>
+        <h2 className=" text-start text-sm">Ingreso total</h2>
+        <p className="text-2xl font-semibold leading-none">$71,813.26</p>
       </div>
       <div className="flex gap-3">
-        <div className="flex gap-1">
-          <div className="w-4">
+        <div className="flex h-4 gap-1">
+          <div className="flex w-4">
             <img
               width={20}
               height={20}
               src="src/assets/expenses.svg"
               alt=""
-              className="self-baseline"
+              className="self-center"
             />
           </div>
-          <span className="self-end font-medium text-pink-500">$1.294</span>
+          <span className="self-end font-medium leading-none text-pink-500">
+            $1.294
+          </span>
         </div>
-        <div className="flex gap-1">
-          <div className="w-4">
+        <div className="flex h-4 gap-1">
+          <div className="flex w-4">
             <img
               width={20}
               height={20}
               src="src/assets/earnings.svg"
               alt=""
-              className="self-baseline"
+              className="self-center"
             />
           </div>
-          <span className="self-end  font-medium text-green-500">$1.294</span>
+          <span className="self-end font-medium  leading-none text-green-500">
+            $1.294
+          </span>
         </div>
       </div>
     </div>
