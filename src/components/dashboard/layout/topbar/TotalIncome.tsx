5b666bd6f874ae2cbb35cbeffbee0a1c4424436d
lugrao <elugrao@gmail.com>
2024-04-30 15:18:43 -0300

    add expenses icon

--- src/components/dashboard/layout/topbar/TotalIncome.tsx
+++ src/components/dashboard/layout/topbar/TotalIncome.tsx
@@ -7,8 +7,16 @@ export const TotalIncome = () => {
       </div>
       <div className="flex gap-3">
         <div className="flex gap-1">
-          <span className="self-end">icon</span>
-          <span className="self-end font-medium text-pink-500">$1.294</span>
+          <div className="w-4">
+            <img
+              width={20}
+              height={20}
+              src="src/assets/expenses.svg"
+              alt=""
+              className="self-baseline"
+            />
+            <span className="self-end font-medium text-pink-500">$1.294</span>
+          </div>
         </div>
         <div className="flex gap-1">
           <span className="self-end">icon</span>
