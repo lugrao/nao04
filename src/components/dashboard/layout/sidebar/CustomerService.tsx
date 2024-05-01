f68190b495ce82a6c050dc2b4f657afe4574cf8b
lugrao <elugrao@gmail.com>
2024-05-01 13:12:27 -0300

    rename icon

--- src/components/dashboard/layout/sidebar/CustomerService.tsx
+++ src/components/dashboard/layout/sidebar/CustomerService.tsx
@@ -13,7 +13,7 @@ export const CustomerService = () => {
           <button className="btn bg-blue-200 font-semibold text-sky-700 hover:bg-blue-300">
             <div>
               <img
-                src="src/assets/atención-al-cliente.svg"
+                src="src/assets/customer-service.svg"
                 alt=""
                 width={23}
                 height={18}
