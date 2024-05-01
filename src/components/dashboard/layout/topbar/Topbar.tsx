5efda57438486e5cc3944aec7c977b80830799b2
lugrao <elugrao@gmail.com>
2024-05-01 11:38:39 -0300

    add SearchBar component

--- src/components/dashboard/layout/topbar/Topbar.tsx
+++ src/components/dashboard/layout/topbar/Topbar.tsx
@@ -1,9 +1,11 @@
 import { TotalIncome } from "./TotalIncome";
+import { SearchBar } from "./SearchBar";
 
 export const Topbar = () => {
   return (
     <div className="h-36 w-full bg-violet-50 ">
       <TotalIncome totalIncome={71813.26} expenses={1294} earnings={1294} />
+      <SearchBar />
     </div>
   );
 };
