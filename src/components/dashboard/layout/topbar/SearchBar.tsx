71d847b4a67ef8dea06d35d2b3275392ae6a5d8d
lugrao <elugrao@gmail.com>
2024-05-01 13:33:34 -0300

    add input with label and icon

--- src/components/dashboard/layout/topbar/SearchBar.tsx
+++ src/components/dashboard/layout/topbar/SearchBar.tsx
@@ -1,3 +1,15 @@
 export const SearchBar = () => {
-  return <div></div>;
+  return (
+    <label className="input flex max-w-screen-sm items-center gap-2">
+      <div className="h-4 w-4">
+        <img
+          width={17}
+          height={17}
+          src="src/assets/search.svg"
+          alt="Ícono de búsqueda"
+        />
+      </div>
+      <input type="text" className="grow" placeholder="Buscar" />
+    </label>
+  );
 };
