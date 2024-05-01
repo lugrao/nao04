76db02c66b75b54bd03e2ecae89628e18bfc2728
lugrao <elugrao@gmail.com>
2024-05-01 13:39:49 -0300

    add wrapper div

--- src/components/dashboard/layout/topbar/SearchBar.tsx
+++ src/components/dashboard/layout/topbar/SearchBar.tsx
@@ -1,15 +1,17 @@
 export const SearchBar = () => {
   return (
-    <label className="input flex max-w-screen-sm items-center gap-2">
-      <div className="h-4 w-4">
-        <img
-          width={17}
-          height={17}
-          src="src/assets/search.svg"
-          alt="Ícono de búsqueda"
-        />
-      </div>
-      <input type="text" className="grow" placeholder="Buscar" />
-    </label>
+    <div className="h-max p-2">
+      <label className="input flex max-w-screen-sm items-center gap-2">
+        <div className="h-4 w-4">
+          <img
+            width={17}
+            height={17}
+            src="src/assets/search.svg"
+            alt="Ícono de búsqueda"
+          />
+        </div>
+        <input type="text" className="grow" placeholder="Buscar" />
+      </label>
+    </div>
   );
 };
