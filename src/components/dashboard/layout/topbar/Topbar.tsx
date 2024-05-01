20079de18487ce97d091d8a1c383dd8633c7f5f8
lugrao <elugrao@gmail.com>
2024-05-01 13:41:51 -0300

    align topbar items horizontally in a row

--- src/components/dashboard/layout/topbar/Topbar.tsx
+++ src/components/dashboard/layout/topbar/Topbar.tsx
@@ -3,7 +3,7 @@ import { SearchBar } from "./SearchBar";
 
 export const Topbar = () => {
   return (
-    <div className="h-36 w-full bg-violet-50 ">
+    <div className="flex h-36 w-full bg-violet-50">
       <TotalIncome totalIncome={71813.26} expenses={1294} earnings={1294} />
       <SearchBar />
     </div>
