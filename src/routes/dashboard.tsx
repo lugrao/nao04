b28b90a3869b9ae9dcac47493c8fc47bf70706c7
lugrao <elugrao@gmail.com>
2024-05-03 19:01:52 -0300

    add Home section

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -1,11 +1,14 @@
 import { Sidebar } from "src/components";
 import { Topbar } from "src/components";
-
+import { Home } from "src/components";
 export const Dashboard = () => {
   return (
     <div className="flex w-full">
       <Sidebar />
-      <Topbar />
+      <div className="flex flex-col w-full">
+        <Topbar />
+        <Home />
+      </div>
     </div>
   );
 };
