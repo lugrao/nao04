d9564253336942fb83382cdbe8c16281bb710840
lugrao <elugrao@gmail.com>
2024-05-03 16:31:17 -0300

    add notification icons

--- src/components/dashboard/layout/topbar/Notifications.tsx
+++ src/components/dashboard/layout/topbar/Notifications.tsx
@@ -1,3 +1,22 @@
 export const Notifications = () => {
-  return <div></div>;
+  return (
+    <div className="flex gap-12">
+      <div className="w-7">
+        <img
+          width={26}
+          height={26}
+          src="src/assets/notification-message.svg"
+          alt="Ícono de notificación de mensajes"
+        />
+      </div>
+      <div className="w-7">
+        <img
+          width={24}
+          height={24}
+          src="src/assets/notification-alert.svg"
+          alt="Ícono de alerta de notificaciones"
+        />
+      </div>
+    </div>
+  );
 };
