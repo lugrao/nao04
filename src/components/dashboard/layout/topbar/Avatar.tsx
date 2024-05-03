3f13380007c0695a330ebb318650c13fef6bf137
lugrao <elugrao@gmail.com>
2024-05-03 18:24:45 -0300

    add min width to avatar

--- src/components/dashboard/layout/topbar/Avatar.tsx
+++ src/components/dashboard/layout/topbar/Avatar.tsx
@@ -1,6 +1,6 @@
 export const Avatar = () => {
   return (
-    <div className="h-min p-2">
+    <div className="h-min min-w-16 p-2">
       <div className="flex justify-center overflow-hidden rounded-full bg-white">
         <img
           width={50}
