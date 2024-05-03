18ae4577cf12716629f172e9c90da1b26fe377ae
lugrao <elugrao@gmail.com>
2024-05-03 18:27:07 -0300

    give items and topbar correct spacing

--- src/components/dashboard/layout/topbar/Topbar.tsx
+++ src/components/dashboard/layout/topbar/Topbar.tsx
@@ -4,11 +4,15 @@ import { Notifications } from "./Notifications";
 import { Avatar } from "./Avatar";
 export const Topbar = () => {
   return (
-    <div className="flex h-36 w-full bg-violet-50">
+    <div className="flex h-min w-full items-center justify-between bg-violet-50 p-5">
       <TotalIncome totalIncome={71813.26} expenses={1294} earnings={1294} />
-      <SearchBar />
-      <Notifications />
-      <Avatar />
+      <div className="w-5/12">
+        <SearchBar />
+      </div>
+      <div className="flex items-center gap-6">
+        <Notifications />
+        <Avatar />
+      </div>
     </div>
   );
 };
