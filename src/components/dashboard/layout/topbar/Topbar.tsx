4d85dd1cd6afb3622dac6e3c3bb9f4a2cc667984
lugrao <elugrao@gmail.com>
2024-05-03 16:31:29 -0300

    import Notifications component

--- src/components/dashboard/layout/topbar/Topbar.tsx
+++ src/components/dashboard/layout/topbar/Topbar.tsx
@@ -1,11 +1,13 @@
 import { TotalIncome } from "./TotalIncome";
 import { SearchBar } from "./SearchBar";
+import { Notifications } from "./Notifications";
 
 export const Topbar = () => {
   return (
     <div className="flex h-36 w-full bg-violet-50">
       <TotalIncome totalIncome={71813.26} expenses={1294} earnings={1294} />
       <SearchBar />
+      <Notifications />
     </div>
   );
 };
