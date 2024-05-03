31176b7656202fea9a9e02a557e0c28f9fa18936
lugrao <elugrao@gmail.com>
2024-05-03 17:03:11 -0300

    add user icon as avatar

--- src/components/dashboard/layout/topbar/Avatar.tsx
+++ src/components/dashboard/layout/topbar/Avatar.tsx
@@ -1,3 +1,14 @@
 export const Avatar = () => {
-  return <div></div>;
+  return (
+    <div className="h-min p-2">
+      <div className="flex justify-center overflow-hidden rounded-full bg-white">
+        <img
+          width={50}
+          height={50}
+          src="src/assets/user.svg"
+          alt="Ícono de usuario"
+        />
+      </div>
+    </div>
+  );
 };
