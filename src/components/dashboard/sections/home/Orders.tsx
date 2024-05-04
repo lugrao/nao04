1f0f84e380704f7ba8386aec8b15043b68f1a240
lugrao <elugrao@gmail.com>
2024-05-04 12:06:05 -0300

    add prop types; set icon and color based on order status

--- src/components/dashboard/sections/home/Orders.tsx
+++ src/components/dashboard/sections/home/Orders.tsx
@@ -1,8 +1,24 @@
-export const Orders = () => {
+export type OrderStatus = "sent" | "pending" | "new";
+
+export interface OrdersProps {
+  status: OrderStatus;
+  amount: number;
+}
+
+const orders: Record<OrderStatus, { color: string; icon: string }> = {
+  sent: { color: "blue", icon: "truck" },
+  pending: { color: "pink", icon: "kart" },
+  new: { color: "purple", icon: "bag" },
+};
+
+export const Orders = ({ status, amount }: OrdersProps) => {
+  const color = orders[status].color;
   return (
-    <div className="relative  h-32 w-72 rounded-2xl bg-gradient-to-br from-blue-400 to-blue-500">
+    <div
+      className={`relative  h-32 w-72 rounded-2xl bg-gradient-to-br from-${color}-400 to-${color}-500`}
+    >
       <div className="absolute bottom-[-0.5rem] left-2 ">
-        <img src="src/assets/truck.svg" alt="Camión" />
+        <img src={`src/assets/${orders[status].icon}.svg`} alt="Camión" />
       </div>
     </div>
   );
