4a415a7510f589c80d4d36de6758aa625a431d40
lugrao <elugrao@gmail.com>
2024-05-04 11:42:37 -0300

    import and render Orders component

--- src/components/dashboard/sections/home/Home.tsx
+++ src/components/dashboard/sections/home/Home.tsx
@@ -1,3 +1,9 @@
+import { Orders } from "./Orders";
+
 export const Home = () => {
-  return <div className="h-full overflow-y-scroll"></div>;
+  return (
+    <div className="h-full overflow-y-scroll">
+      <Orders />
+    </div>
+  );
 };
