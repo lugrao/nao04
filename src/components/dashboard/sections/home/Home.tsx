9dad64a99230375011a5a051e991a4a18ec809d3
lugrao <elugrao@gmail.com>
2024-05-04 13:37:51 -0300

    add pending and new orders

--- src/components/dashboard/sections/home/Home.tsx
+++ src/components/dashboard/sections/home/Home.tsx
@@ -4,6 +4,8 @@ export const Home = () => {
   return (
     <div className="h-full overflow-y-scroll">
       <Orders status="sent" amount={39} />
+      <Orders status="pending" amount={15} />
+      <Orders status="new" amount={28} />
     </div>
   );
 };
