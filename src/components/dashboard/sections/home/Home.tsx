0867e23f5100401ed78bc3a6f3ac1d5c3672cfcc
lugrao <elugrao@gmail.com>
2024-05-04 14:05:05 -0300

    horizontally align Orders components and add correct spacing between them

--- src/components/dashboard/sections/home/Home.tsx
+++ src/components/dashboard/sections/home/Home.tsx
@@ -3,9 +3,11 @@ import { Orders } from "./Orders";
 export const Home = () => {
   return (
     <div className="h-full overflow-y-scroll">
-      <Orders status="sent" amount={39} />
-      <Orders status="pending" amount={15} />
-      <Orders status="new" amount={28} />
+      <div className="flex justify-evenly gap-3 px-4 py-10">
+        <Orders status="sent" amount={39} />
+        <Orders status="pending" amount={15} />
+        <Orders status="new" amount={28} />
+      </div>
     </div>
   );
 };
