bdd2ab082ba80f2d2f7c57cf8fbc0424f7c61106
lugrao <elugrao@gmail.com>
2024-05-04 17:57:59 -0300

    add logic for chaning sections

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import { Sidebar } from "src/components";
 import { Topbar } from "src/components";
 import { Home } from "src/components";
+
 export const Dashboard = () => {
+  const [activeSection, setActiveSection] = useState("dashboard");
   return (
     <div className="flex w-full">
-      <Sidebar />
-      <div className="flex flex-col w-full">
+      <Sidebar setActiveSection={setActiveSection} />
+      <div className="flex w-full flex-col">
         <Topbar />
-        <Home />
+        {activeSection === "dashboard" && <Home />}
       </div>
     </div>
   );
