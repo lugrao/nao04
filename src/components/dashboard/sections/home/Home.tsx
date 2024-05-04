b316c82c82b8a1f7a604cfedcc418dfcaff76c4c
lugrao <elugrao@gmail.com>
2024-05-04 12:04:52 -0300

    add orders status and amount to Orders component

--- src/components/dashboard/sections/home/Home.tsx
+++ src/components/dashboard/sections/home/Home.tsx
@@ -3,7 +3,7 @@ import { Orders } from "./Orders";
 export const Home = () => {
   return (
     <div className="h-full overflow-y-scroll">
-      <Orders />
+      <Orders status="sent" amount={39} />
     </div>
   );
 };
