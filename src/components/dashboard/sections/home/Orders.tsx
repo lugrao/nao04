7c42493e91e88b2793956b2f81314dc46267f2f1
lugrao <elugrao@gmail.com>
2024-05-04 11:43:50 -0300

    add truck icon and background color

--- src/components/dashboard/sections/home/Orders.tsx
+++ src/components/dashboard/sections/home/Orders.tsx
@@ -1,3 +1,9 @@
 export const Orders = () => {
-  return <div></div>;
+  return (
+    <div className="relative  h-32 w-72 rounded-2xl bg-gradient-to-br from-blue-400 to-blue-500">
+      <div className="absolute bottom-[-0.5rem] left-2 ">
+        <img src="src/assets/truck.svg" alt="Camión" />
+      </div>
+    </div>
+  );
 };
