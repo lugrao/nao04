6256467ed8b933facbdf6f2404165abfaf73509c
lugrao <elugrao@gmail.com>
2024-05-06 15:46:38 -0300

    conditionally add shadow to the bottom

--- src/components/dashboard/layout/topbar/Topbar.tsx
+++ src/components/dashboard/layout/topbar/Topbar.tsx
@@ -2,9 +2,16 @@ import { TotalIncome } from "./TotalIncome";
 import { SearchBar } from "./SearchBar";
 import { Notifications } from "./Notifications";
 import { Avatar } from "./Avatar";
-export const Topbar = () => {
+
+export interface TopbarProps {
+  shadowBottom: boolean;
+}
+
+export const Topbar = ({ shadowBottom }: TopbarProps) => {
   return (
-    <div className="flex h-min w-full items-center justify-between bg-violet-50 p-5">
+    <div
+      className={`${shadowBottom && "shadow-bottom"} z-10 flex h-min w-full items-center justify-between bg-violet-50 p-5`}
+    >
       <TotalIncome totalIncome={71813.26} expenses={1294} earnings={1294} />
       <div className="w-5/12">
         <SearchBar />
