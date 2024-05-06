821e69fa8d5580bc6619c8585f9e72a5759ee226
lugrao <elugrao@gmail.com>
2024-05-06 14:53:03 -0300

    add chart props and chart dictionary for selecting colors

--- src/components/dashboard/sections/home/LineChart.tsx
+++ src/components/dashboard/sections/home/LineChart.tsx
@@ -2,19 +2,32 @@ import { data } from "./chartData";
 import { ResponsiveLine } from "@nivo/line";
 import { formatUSDAmount } from "utils/formatUSDAmount";
 
-export const LineChart = () => {
+export type ChartCategory = "sales" | "customers";
+
+export interface LineChartProps {
+  category: ChartCategory;
+  title: string;
+  totalAmount: number;
+}
+
+const chart = {
+  sales: { color: { amount: "text-purple-500", lines: "#c084fc" } },
+  customers: { color: { amount: "text-sky-400", lines: "#7dd3fc" } },
+};
+
+export const LineChart = ({ category, title, totalAmount }: LineChartProps) => {
   return (
     <div className="flex h-[26rem] w-5/12 flex-col justify-center rounded-2xl bg-white p-6">
       <div className="flex items-end justify-between pt-4">
-        <h2 className="text-lg font-bold leading-none">Ventas 2024</h2>
-        <h3 className="text-2xl font-semibold leading-none text-purple-500">
-          {formatUSDAmount(12434.52)}
+        <h2 className="text-lg font-bold leading-none">{title}</h2>
+        <h3 className={`text-2xl font-semibold leading-none ${chart[category].color.amount}`}>
+          {category === "sales" ? formatUSDAmount(totalAmount) : totalAmount}
         </h3>
         <span className="text-sm leading-none text-gray-400">27 Abr 2024</span>
       </div>
       <ResponsiveLine
         data={data}
-        colors="#c084fc"
+        colors={chart[category].color.lines}
         margin={{ top: 30, right: 20, bottom: 30, left: 50 }}
         xScale={{ type: "point" }}
         yScale={{
@@ -48,8 +61,8 @@ export const LineChart = () => {
         }}
         lineWidth={2}
         pointSize={6}
-        pointColor="#c084fc"
-        pointBorderColor="#c084fc"
+        pointColor={chart[category].color.lines}
+        pointBorderColor={chart[category].color.lines}
         pointBorderWidth={2}
         pointLabel="data.yFormatted"
         pointLabelYOffset={-12}
