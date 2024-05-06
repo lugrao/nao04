17c97b82eb20dd03888d23f6633e821341f30098
lugrao <elugrao@gmail.com>
2024-05-06 15:48:33 -0300

    pass handleScroll function to Home component

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -2,15 +2,17 @@ import { useState } from "react";
 import { Sidebar } from "src/components";
 import { Topbar } from "src/components";
 import { Home } from "src/components";
+import { useIsScrolled } from "src/utils/useIsScrolled";
 
 export const Dashboard = () => {
+  const [isScrolled, handleScroll] = useIsScrolled();
   const [activeSection, setActiveSection] = useState("dashboard");
   return (
     <div className="flex w-full">
       <Sidebar setActiveSection={setActiveSection} />
       <div className="flex w-full flex-col">
-        <Topbar />
-        {activeSection === "dashboard" && <Home />}
+        <Topbar shadowBottom={isScrolled} />
+        {activeSection === "dashboard" && <Home onScroll={handleScroll} />}
       </div>
     </div>
   );
