c29537366204f72282e184942d0a217b41ee081a
lugrao <elugrao@gmail.com>
2024-05-07 12:01:59 -0300

    add best selling products table data

---
+++ src/components/dashboard/sections/home/bestSellingProductsData.ts
@@ -0,0 +1,6 @@
+export const data = [
+  { productName: "Producto A", salesPercentage: 35 },
+  { productName: "Producto B", salesPercentage: 7 },
+  { productName: "Producto C", salesPercentage: -4 },
+  { productName: "Producto H", salesPercentage: 12 },
+];
