9d7549ed63d01790c20f5bddfdd22eb93b6926cd
lugrao <elugrao@gmail.com>
2024-05-07 15:13:55 -0300

    import and export Sales component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import { Sidebar } from "components/dashboard/layout/sidebar/Sidebar.tsx";
 import { Topbar } from "components/dashboard/layout/topbar/Topbar.tsx";
-import { Home } from "./dashboard/sections/home/Home";
+import { Home } from "components/dashboard/sections/home/Home";
+import { Sales } from "components/dashboard/sections/sales/Sales";
 
-export { Sidebar, Topbar, Home };
+export { Sidebar, Topbar, Home, Sales };
