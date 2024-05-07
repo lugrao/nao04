c13a9b1e6037d0b0af1d2c4083593d81d449d1f5
lugrao <elugrao@gmail.com>
2024-05-07 12:01:07 -0300

    import BestSellingProductTable and integrate it with charts

--- src/components/dashboard/sections/home/Home.tsx
+++ src/components/dashboard/sections/home/Home.tsx
@@ -1,5 +1,7 @@
 import { Orders } from "./Orders";
 import { LineChart } from "./LineChart";
+import { BestSellingProductsTable } from "./BestSellingProductsTable";
+import { data } from "./bestSellingProductsData";
 
 export interface HomeProps {
   onScroll: (e: React.UIEvent<HTMLDivElement>) => void;
@@ -8,19 +10,23 @@ export interface HomeProps {
 export const Home = ({ onScroll }: HomeProps) => {
   return (
     <div className="h-full overflow-y-scroll" onScroll={onScroll}>
-      <div className="flex justify-evenly gap-3 px-4 py-10">
+      <div className="flex justify-center gap-10 px-4 py-10">
         <Orders status="sent" amount={39} />
         <Orders status="pending" amount={15} />
         <Orders status="new" amount={28} />
       </div>
-
-      <div className="flex flex-col gap-10">
-        <LineChart
-          category="sales"
-          totalAmount={12434.53}
-          title="Ventas 2024"
-        />
-        <LineChart category="customers" totalAmount={1613} title="Clientes" />
+      <div className="flex justify-center gap-10 px-5 py-10">
+        <div className="flex flex-col gap-10">
+          <LineChart
+            category="sales"
+            totalAmount={12434.53}
+            title="Ventas 2024"
+          />
+          <LineChart category="customers" totalAmount={1613} title="Clientes" />
+        </div>
+        <div className="flex flex-col gap-10">
+          <BestSellingProductsTable data={data} />
+        </div>
       </div>
     </div>
   );
