f13cdae943f1af23be5bec071796796715da3981
lugrao <elugrao@gmail.com>
2024-05-07 14:37:10 -0300

    add customers chart data

--- src/components/dashboard/sections/home/chartData.ts
+++ src/components/dashboard/sections/home/chartData.ts
@@ -1,9 +1,10 @@
-interface ChartData {
+export interface ChartData {
   id: string;
   color: string;
   data: { x: string; y: number }[];
 }
-export const data: ChartData[] = [
+
+export const salesData: ChartData[] = [
   {
     id: "2024",
     color: "hsl(177, 70%, 50%)",
@@ -27,6 +28,35 @@ export const data: ChartData[] = [
       {
         x: "Mayo",
         y: 152321,
+      },
+    ],
+  },
+];
+
+export const customersData: ChartData[] = [
+  {
+    id: "2024",
+    color: "hsl(177, 70%, 50%)",
+    data: [
+      {
+        x: "2020",
+        y: 10,
+      },
+      {
+        x: "2021",
+        y: 423,
+      },
+      {
+        x: "2022",
+        y: 1042,
+      },
+      {
+        x: "2023",
+        y: 1329,
+      },
+      {
+        x: "2024",
+        y: 1407,
       },
     ],
   },
