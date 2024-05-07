918a88861b944acf9a5a4fc530e4079da2c12c03
lugrao <elugrao@gmail.com>
2024-05-07 14:13:42 -0300

    add data for LatestSalesTable

---
+++ src/components/dashboard/sections/home/latestSalesData.ts
@@ -0,0 +1,8 @@
+export const data = [
+  { productName: "Producto A", saleAmount: 122.54 },
+  { productName: "Producto B", saleAmount: 22.41 },
+  { productName: "Producto C", saleAmount: 232.99 },
+  { productName: "Producto D", saleAmount: 431.04 },
+  { productName: "Producto E", saleAmount: 19.76 },
+  { productName: "Producto F", saleAmount: 90.01 },
+];
