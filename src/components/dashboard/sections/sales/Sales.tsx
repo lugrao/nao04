e1b486997be673c63b67038d3312ca21a5ec1f40
lugrao <elugrao@gmail.com>
2024-05-08 15:48:19 -0300

    add onScroll prop

--- src/components/dashboard/sections/sales/Sales.tsx
+++ src/components/dashboard/sections/sales/Sales.tsx
@@ -1,9 +1,10 @@
 import { WorldMap } from "./WorldMap";
 import { SalesByCountryTable } from "./SalesByCountryTable";
 import { data } from "./salesByCountryTableData";
+import { SectionProps } from "../home/Home";
 
-export const Sales = () => (
-  <div className="flex flex-col items-center gap-10 overflow-scroll px-4 py-10">
+export const Sales = ({ onScroll }: SectionProps) => (
+  <div className="flex flex-col items-center gap-10 overflow-scroll px-4 py-10" onScroll={onScroll}>
     <div>
       <WorldMap data={data} />
     </div>
