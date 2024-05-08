8ee0ef6f271f5b466cefc55848f7d454c3d7475c
lugrao <elugrao@gmail.com>
2024-05-08 15:44:48 -0300

    use SalesByCountryTable data and map it to map's data

--- src/components/dashboard/sections/sales/WorldMap.tsx
+++ src/components/dashboard/sections/sales/WorldMap.tsx
@@ -1,47 +1,77 @@
+import { useState, useEffect } from "react";
 import { ResponsiveChoropleth } from "@nivo/geo";
-import { data } from "./worldMapData";
 import worldCountries from "./worldCountries.json";
+import { SalesByCountryTableData } from "./SalesByCountryTable";
 
-export const WorldMap = () => (
-  <div className="w-[65rem] rounded-2xl  bg-white px-10 lg:h-[45rem] 2xl:h-[40rem]">
-    <ResponsiveChoropleth
-      data={data}
-      features={worldCountries.features}
-      margin={{ top: 50, right: 0, bottom: 0, left: 20 }}
-      colors="purples"
-      domain={[0, 1000000]}
-      unknownColor="#666666"
-      label="properties.name"
-      valueFormat=".2s"
-      projectionTranslation={[0.5, 0.5]}
-      projectionRotation={[0, 0, 0]}
-      borderWidth={0.5}
-      borderColor="#152538"
-      legends={[
-        {
-          anchor: "bottom-left",
-          direction: "column",
-          justify: true,
-          translateX: 20,
-          translateY: -100,
-          itemsSpacing: 0,
-          itemWidth: 94,
-          itemHeight: 18,
-          itemDirection: "left-to-right",
-          itemTextColor: "#444444",
-          itemOpacity: 0.85,
-          symbolSize: 18,
-          effects: [
-            {
-              on: "hover",
-              style: {
-                itemTextColor: "#000000",
-                itemOpacity: 1,
+export interface WorldMapProps {
+  data: SalesByCountryTableData;
+}
+
+export const WorldMap = ({ data }: WorldMapProps) => {
+  const [mapData, setMapData] = useState<
+    { id: string; value: number }[] | undefined
+  >(undefined);
+
+  console.log(mapData);
+
+  useEffect(() => {
+    const mData: { id: string; value: number }[] = [];
+
+    data.forEach((item) =>
+      mData.push({
+        id: item.countryCode,
+        value: item.totalIncome,
+      }),
+    );
+    setMapData(mData);
+  }, [data]);
+
+  if (mapData === undefined)
+    return (
+      <div className="w-[65rem] rounded-2xl  bg-white px-10 shadow-md lg:h-[45rem] 2xl:h-[40rem]"></div>
+    );
+
+  return (
+    <div className="w-[65rem] rounded-2xl  bg-white px-10 shadow-md lg:h-[45rem] 2xl:h-[40rem]">
+      <ResponsiveChoropleth
+        data={mapData}
+        features={worldCountries.features}
+        margin={{ top: 50, right: 0, bottom: 0, left: 20 }}
+        colors="purples"
+        domain={[0, 1000000]}
+        unknownColor="#666666"
+        label="properties.name"
+        valueFormat=".2s"
+        projectionTranslation={[0.5, 0.5]}
+        projectionRotation={[0, 0, 0]}
+        borderWidth={0.5}
+        borderColor="#152538"
+        legends={[
+          {
+            anchor: "bottom-left",
+            direction: "column",
+            justify: true,
+            translateX: 20,
+            translateY: -100,
+            itemsSpacing: 0,
+            itemWidth: 94,
+            itemHeight: 18,
+            itemDirection: "left-to-right",
+            itemTextColor: "#444444",
+            itemOpacity: 0.85,
+            symbolSize: 18,
+            effects: [
+              {
+                on: "hover",
+                style: {
+                  itemTextColor: "#000000",
+                  itemOpacity: 1,
+                },
               },
-            },
-          ],
-        },
-      ]}
-    />
-  </div>
-);
+            ],
+          },
+        ]}
+      />
+    </div>
+  );
+};
