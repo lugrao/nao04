7150d345f4fbd5dc564eff78df91151690ca4b53
lugrao <elugrao@gmail.com>
2024-05-08 18:52:45 -0300

    import and render InvoicesTable, with data

--- src/components/dashboard/sections/invoices/Invoices.tsx
+++ src/components/dashboard/sections/invoices/Invoices.tsx
@@ -1,5 +1,9 @@
 import { SectionProps } from "../home/Home";
+import { InvoicesTable } from "./InvoicesTable";
+import { data } from "./invoicesTableData";
 
 export const Invoices = ({ onScroll }: SectionProps) => (
-  <div onScroll={onScroll}></div>
+  <div onScroll={onScroll}>
+    <InvoicesTable data={data} />
+  </div>
 );
