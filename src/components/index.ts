bf9d447b2ed51592fbb1483985982d4a54371890
lugrao <elugrao@gmail.com>
2024-05-08 17:08:35 -0300

    import and export Invoices component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,6 @@ import { Topbar } from "components/dashboard/layout/topbar/Topbar.tsx";
 import { Home } from "components/dashboard/sections/home/Home";
 import { Sales } from "components/dashboard/sections/sales/Sales";
 import { Products } from "components/dashboard/sections/products/Products";
+import { Invoices } from "components/dashboard/sections/invoices/Invoices";
 
-export { Sidebar, Topbar, Home, Sales, Products };
+export { Sidebar, Topbar, Home, Sales, Products, Invoices };
