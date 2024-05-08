52433baef79a0241ddee78219bbd9cda23677afb
lugrao <elugrao@gmail.com>
2024-05-08 19:21:40 -0300

    center items and add padding

--- src/components/dashboard/sections/invoices/Invoices.tsx
+++ src/components/dashboard/sections/invoices/Invoices.tsx
@@ -3,7 +3,10 @@ import { InvoicesTable } from "./InvoicesTable";
 import { data } from "./invoicesTableData";
 
 export const Invoices = ({ onScroll }: SectionProps) => (
-  <div onScroll={onScroll}>
+  <div
+    className="flex flex-col items-center overflow-scroll px-4 py-10"
+    onScroll={onScroll}
+  >
     <InvoicesTable data={data} />
   </div>
 );
