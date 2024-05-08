429a752a1e84be26284b1300c068deee07bc147e
lugrao <elugrao@gmail.com>
2024-05-08 17:11:17 -0300

    add onScroll prop and pass function to component

--- src/components/dashboard/sections/invoices/Invoices.tsx
+++ src/components/dashboard/sections/invoices/Invoices.tsx
@@ -1,1 +1,5 @@
-export const Invoices = () => <div></div>;
+import { SectionProps } from "../home/Home";
+
+export const Invoices = ({ onScroll }: SectionProps) => (
+  <div onScroll={onScroll}></div>
+);
