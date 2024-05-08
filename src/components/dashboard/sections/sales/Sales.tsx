808b279be06d22ff6fcc0a512375c074c7455841
lugrao <elugrao@gmail.com>
2024-05-08 15:40:42 -0300

    add padding; add wrapper div to WorldMap component

--- src/components/dashboard/sections/sales/Sales.tsx
+++ src/components/dashboard/sections/sales/Sales.tsx
@@ -3,8 +3,10 @@ import { SalesByCountryTable } from "./SalesByCountryTable";
 import { data } from "./salesByCountryTableData";
 
 export const Sales = () => (
-  <div className="flex flex-col items-center overflow-scroll gap-10">
-    <WorldMap />
+  <div className="flex flex-col items-center gap-10 overflow-scroll px-4 py-10">
+    <div>
+      <WorldMap data={data} />
+    </div>
     <SalesByCountryTable data={data} />
   </div>
 );
