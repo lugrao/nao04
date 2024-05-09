064c34fd7530511f462695dcf91f89fb9cf72237
lugrao <elugrao@gmail.com>
2024-05-09 13:43:12 -0300

    display map only in desktop

--- src/components/dashboard/sections/sales/Sales.tsx
+++ src/components/dashboard/sections/sales/Sales.tsx
@@ -3,11 +3,20 @@ import { SalesByCountryTable } from "./SalesByCountryTable";
 import { data } from "./salesByCountryTableData";
 import { SectionProps } from "../home/Home";
 
-export const Sales = ({ onScroll }: SectionProps) => (
-  <div className="flex flex-col items-center gap-10 overflow-scroll px-4 py-10" onScroll={onScroll}>
-    <div>
-      <WorldMap data={data} />
-    </div>
+export const Sales = ({
+  onScroll,
+  viewportWidth,
+}: SectionProps & { viewportWidth: number }) => (
+  <div
+    className="flex flex-col items-center gap-10 overflow-scroll px-4 py-10"
+    onScroll={onScroll}
+  >
+    {viewportWidth > 767 && (
+      <div>
+        <WorldMap data={data} />
+      </div>
+    )}
+
     <SalesByCountryTable data={data} />
   </div>
 );
