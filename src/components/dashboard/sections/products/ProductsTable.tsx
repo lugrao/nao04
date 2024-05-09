06c0d80cdeed5561cf4fba268fd55ce831bb5185
lugrao <elugrao@gmail.com>
2024-05-09 13:48:49 -0300

    adjust width for mobile devices

--- src/components/dashboard/sections/products/ProductsTable.tsx
+++ src/components/dashboard/sections/products/ProductsTable.tsx
@@ -12,7 +12,7 @@ export interface ProductsTableProps {
 }
 
 export const ProductsTable = ({ data }: ProductsTableProps) => (
-  <div className="w-[70rem] rounded-2xl bg-white pb-4 shadow-md">
+  <div className="w-full max-w-[70rem] rounded-2xl bg-white pb-4 shadow-md">
     <div className="flex justify-between pb-7 pl-11 pt-7">
       <h2 className="text-lg font-bold">Productos</h2>
     </div>
