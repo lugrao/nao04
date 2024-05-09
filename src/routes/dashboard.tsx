861945b34db79db2f8c3a644f0973856f9cf7231
lugrao <elugrao@gmail.com>
2024-05-09 11:34:46 -0300

    hide sidebar when mobile

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -6,20 +6,25 @@ import { Sales } from "src/components";
 import { Products } from "src/components";
 import { Invoices } from "src/components";
 import { useIsScrolled } from "src/utils/useIsScrolled";
+import { useWindowDimensions } from "src/utils/useWindowDimensions";
 
 export const Dashboard = () => {
+  const { width } = useWindowDimensions();
   const [isScrolled, handleScroll] = useIsScrolled();
   const [activeSection, setActiveSection] = useState("dashboard");
   return (
-    <div className="flex w-full">
-      <Sidebar setActiveSection={setActiveSection} />
-      <div className="flex w-full flex-col">
-        <Topbar shadowBottom={isScrolled} />
-        {activeSection === "dashboard" && <Home onScroll={handleScroll} />}
-        {activeSection === "sales" && <Sales onScroll={handleScroll} />}
-        {activeSection === "products" && <Products onScroll={handleScroll} />}
-        {activeSection === "invoices" && <Invoices onScroll={handleScroll} />}
+    width && (
+      <div className="flex w-full">
+        {width > 1024 && <Sidebar setActiveSection={setActiveSection} />}
+
+        <div className="flex w-full flex-col">
+          <Topbar shadowBottom={isScrolled} />
+          {activeSection === "dashboard" && <Home onScroll={handleScroll} />}
+          {activeSection === "sales" && <Sales onScroll={handleScroll} />}
+          {activeSection === "products" && <Products onScroll={handleScroll} />}
+          {activeSection === "invoices" && <Invoices onScroll={handleScroll} />}
+        </div>
       </div>
-    </div>
+    )
   );
 };
