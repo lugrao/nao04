ea5057d9ff9deeb4fd66df4202c5746066b9024e
lugrao <elugrao@gmail.com>
2024-05-09 14:52:19 -0300

    add custom media query

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   plugins: [require("daisyui")],
+  theme: {
+    extend: {
+      screens: {
+        "xs-max": { raw: "(max-width: 410px)" },
+      },
+    },
+  },
 };
