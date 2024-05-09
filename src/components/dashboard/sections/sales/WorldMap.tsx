15c9484b6b4b50bad0452f8ffe70ab2b15066d99
lugrao <elugrao@gmail.com>
2024-05-09 17:58:27 -0300

    adjust worldmap size at different breakpoints

--- src/components/dashboard/sections/sales/WorldMap.tsx
+++ src/components/dashboard/sections/sales/WorldMap.tsx
@@ -28,11 +28,11 @@ export const WorldMap = ({ data }: WorldMapProps) => {
 
   if (mapData === undefined)
     return (
-      <div className="rounded-2xl bg-white px-10 shadow-md  md:w-[75rem] lg:h-[50rem] lg:w-[75rem] 2xl:h-[40rem] 2xl:w-[65rem]"></div>
+      <div className="rounded-2xl bg-white px-10 shadow-md  lg:h-[54rem] lg:w-[75rem] xl:h-[50rem] 2xl:h-[40rem] 2xl:w-[65rem]"></div>
     );
 
   return (
-    <div className="rounded-2xl bg-white px-10 shadow-md  md:w-[75rem] lg:h-[50rem] lg:w-[75rem] 2xl:h-[40rem] 2xl:w-[65rem]">
+    <div className="rounded-2xl bg-white px-10 shadow-md  lg:h-[54rem] lg:w-[75rem] xl:h-[50rem] 2xl:h-[40rem] 2xl:w-[65rem]">
       <ResponsiveChoropleth
         data={mapData}
         features={worldCountries.features}
