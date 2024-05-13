86bd5f3d6e05d3915aa966be80bd828f37232a69
lugrao <elugrao@gmail.com>
2024-05-13 14:31:40 -0300

    create SalesProps

--- src/components/dashboard/sections/sales/Sales.tsx
+++ src/components/dashboard/sections/sales/Sales.tsx
@@ -3,10 +3,11 @@ import { SalesByCountryTable } from "./SalesByCountryTable";
 import { data } from "./salesByCountryTableData";
 import { SectionProps } from "../home/Home";
 
-export const Sales = ({
-  onScroll,
-  viewportWidth,
-}: SectionProps & { viewportWidth: number }) => (
+export interface SalesProps extends SectionProps {
+  viewportWidth: number;
+}
+
+export const Sales = ({ onScroll, viewportWidth }: SalesProps) => (
   <div
     className="flex flex-col items-center gap-10 overflow-scroll px-4 py-10"
     onScroll={onScroll}
