23ffff920e0c37f0a4761ceb0d8f93c1dab709d9
lugrao <elugrao@gmail.com>
2024-05-14 17:25:04 -0300

    render Outlet component for routing

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,4 +1,5 @@
 import "routes/root.css";
+import { Outlet } from "react-router-dom";
 import { Dashboard } from "routes/dashboard";
 
 /**
@@ -12,7 +13,9 @@ export const Root = (): JSX.Element => {
     <div className="flex h-full w-full items-center justify-center p-2">
       <div className="flex h-full w-full rounded-3xl bg-white p-0.5">
         <div className="flex h-full w-full overflow-hidden rounded-3xl bg-violet-50">
-          <Dashboard />
+          <Dashboard>
+            <Outlet />
+          </Dashboard>
         </div>
       </div>
     </div>
