69379ed5fc184a529a9c41e2633a8eb47d9bd7e0
lugrao <elugrao@gmail.com>
2024-05-14 17:24:03 -0300

    add router and routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Root } from "routes/root.js";
+import { Home } from "./components";
+import { Sales } from "./components";
+import { Products } from "./components";
+import { Invoices } from "./components";
 import "src/index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <div>Error.</div>,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "sales",
+        element: <Sales />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "invoices",
+        element: <Invoices />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Root />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
