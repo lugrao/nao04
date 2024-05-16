e553adbb8b764d33c382b603a59e221febfbf818
lugrao <elugrao@gmail.com>
2024-05-16 13:21:29 -0300

    show map skeleton when map is loading

--- src/components/dashboard/sections/sales/Sales.tsx
+++ src/components/dashboard/sections/sales/Sales.tsx
@@ -18,9 +18,15 @@ export const Sales = (): JSX.Element => {
       className="flex flex-col items-center gap-10 overflow-scroll px-4 py-10"
       onScroll={handleScroll}
     >
-      {width && width > 1024 && (
+      {width ? (
+        width > 1024 && (
+          <div>
+            <WorldMap data={data} />
+          </div>
+        )
+      ) : (
         <div>
-          <WorldMap data={data} />
+          <div className="rounded-2xl bg-white px-10 shadow-md  lg:h-[54rem] lg:w-[75rem] xl:h-[50rem] 2xl:h-[40rem] 2xl:w-[65rem]"></div>
         </div>
       )}
 
