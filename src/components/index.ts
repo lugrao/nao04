0ad186962f803d140b2cc73c0a9b92cd385a0b3c
lugrao <elugrao@gmail.com>
2024-05-18 18:18:29 -0300

    import and export LoginForm, SignUpForm, and CompanyLogo components

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,5 +4,18 @@ import { Home } from "components/dashboard/sections/home/Home";
 import { Sales } from "components/dashboard/sections/sales/Sales";
 import { Products } from "components/dashboard/sections/products/Products";
 import { Invoices } from "components/dashboard/sections/invoices/Invoices";
+import { LoginForm } from "./authentication/LoginForm";
+import { SignUpForm } from "./authentication/SignUpForm";
+import { CompanyLogo } from "./authentication/CompanyLogo";
 
-export { Sidebar, Topbar, Home, Sales, Products, Invoices };
+export {
+  Sidebar,
+  Topbar,
+  Home,
+  Sales,
+  Products,
+  Invoices,
+  LoginForm,
+  SignUpForm,
+  CompanyLogo,
+};
