9553f61b7d84b52b60ddf3dcd83be22d8a19c012
lugrao <elugrao@gmail.com>
2024-05-18 18:17:51 -0300

    add inputs and button; add styles

--- src/components/authentication/LoginForm.tsx
+++ src/components/authentication/LoginForm.tsx
@@ -1,1 +1,16 @@
-export const LoginForm = () => <div></div>;
+import { FormInput } from "./FormInput";
+import { FormButton } from "./FormButton";
+
+export const LoginForm = () => (
+  <div className="flex w-full max-w-sm flex-col items-center gap-10 bg-white">
+    <h1 className="text-2xl font-bold">Iniciar sesión</h1>
+    <form
+      action=""
+      className="flex w-full max-w-[50rem] flex-col items-center gap-6"
+    >
+      <FormInput labelText="Correo electrónico" errorMessage="Error" />
+      <FormInput labelText="Contraseña" errorMessage="Error" />
+      <FormButton />
+    </form>
+  </div>
+);
