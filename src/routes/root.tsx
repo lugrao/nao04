224035c5de25ae9a75eebed9e4758410fca0d1a4
lugrao <elugrao@gmail.com>
2024-05-18 18:21:23 -0300

    render dashboard or login form depending on user login status

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,6 +1,9 @@
 import "routes/root.css";
+import { useAppSelector } from "src/redux/hooks";
+import { selectIsLoggedIn } from "src/components/authentication/userSlice";
 import { Outlet } from "react-router-dom";
 import { Dashboard } from "routes/dashboard";
+import { Login } from "./login";
 
 /**
  * Renders the root component which serves as the main layout container.
@@ -8,14 +11,21 @@ import { Dashboard } from "routes/dashboard";
  * @component
  * @returns {JSX.Element} The rendered Root component.
  */
-export const Root = (): JSX.Element => (
-  <div className="flex h-full w-full items-center justify-center p-2">
-    <div className="flex h-full w-full rounded-3xl bg-white p-0.5">
-      <div className="flex h-full w-full overflow-hidden rounded-3xl bg-violet-50">
-        <Dashboard>
-          <Outlet />
-        </Dashboard>
+export const Root = (): JSX.Element => {
+  const isLoggedIn = useAppSelector(selectIsLoggedIn);
+  return (
+    <div className="flex h-full w-full items-center justify-center p-2">
+      <div className="flex h-full w-full rounded-3xl bg-white p-0.5">
+        <div className="flex h-full w-full overflow-hidden rounded-3xl bg-violet-50">
+          {isLoggedIn ? (
+            <Dashboard>
+              <Outlet />
+            </Dashboard>
+          ) : (
+            <Login />
+          )}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
