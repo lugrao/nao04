06cae0d1e3a84353b42d9ba0bcdbc929c66aac7a
lugrao <elugrao@gmail.com>
2024-05-18 18:17:13 -0300

    add company logo image

--- src/components/authentication/CompanyLogo.tsx
+++ src/components/authentication/CompanyLogo.tsx
@@ -1,1 +1,9 @@
-export const CompanyLogo = () => <div></div>;
+export const CompanyLogo = () => (
+  <div className="h-full">
+    <img
+      className="h-full w-full"
+      src="/capsule-corp-with-text.svg"
+      alt="Logo de Capsule Corporation"
+    />
+  </div>
+);
