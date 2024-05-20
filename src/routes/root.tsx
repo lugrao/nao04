262e9639b7e49f774e5706d0c6b9f645e0d5b2fe
lugrao <elugrao@gmail.com>
2024-05-20 19:40:15 -0300

    add logic for managing authenticated and unauthenticated requests

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,9 +1,8 @@
 import "routes/root.css";
 import { useAppSelector } from "src/redux/hooks";
 import { selectIsLoggedIn } from "src/components/authentication/userSlice";
-import { Outlet } from "react-router-dom";
-import { Dashboard } from "routes/dashboard";
-import { Login } from "./login";
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 /**
  * Renders the root component which serves as the main layout container.
@@ -13,17 +12,49 @@ import { Login } from "./login";
  */
 export const Root = (): JSX.Element => {
   const isLoggedIn = useAppSelector(selectIsLoggedIn);
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    switch (pathname) {
+      case "/":
+        if (isLoggedIn) {
+          navigate("/dashboard");
+        } else {
+          navigate("/login");
+        }
+        break;
+      case "/login":
+        if (isLoggedIn) {
+          navigate("/dashboard");
+        } else {
+          navigate("/login");
+        }
+        break;
+      case "/signup":
+        if (isLoggedIn) {
+          navigate("/dashboard");
+        } else {
+          navigate("/signup");
+        }
+        break;
+      default:
+        if (isLoggedIn) {
+          navigate(pathname);
+        } else {
+          navigate("/login");
+        }
+        break;
+    }
+    setIsLoading(false);
+  }, []);
+
   return (
     <div className="flex h-full w-full items-center justify-center p-2">
       <div className="flex h-full w-full rounded-3xl bg-white p-0.5">
         <div className="flex h-full w-full overflow-hidden rounded-3xl bg-violet-50">
-          {isLoggedIn ? (
-            <Dashboard>
-              <Outlet />
-            </Dashboard>
-          ) : (
-            <Login />
-          )}
+          {!isLoading && <Outlet />}
         </div>
       </div>
     </div>
