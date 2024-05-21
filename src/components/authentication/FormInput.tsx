a330cbcdf4e4c71fe0b9502849c93b2c7349dc0f
lugrao <elugrao@gmail.com>
2024-05-21 11:02:52 -0300

    adjust height; make error message invisible

--- src/components/authentication/FormInput.tsx
+++ src/components/authentication/FormInput.tsx
@@ -8,9 +8,9 @@ export const FormInput = ({ labelText, errorMessage }: FormInputProps) => (
     <div className="label">
       <span className="label-text text-base font-normal">{labelText}</span>
     </div>
-    <input type="text" className="input input-bordered w-full" />
+    <input type="text" className="input input-bordered w-full h-14" />
     <div className="label">
-      <span className="label-text-alt">{errorMessage}</span>
+      <span className="invisible label-text-alt">{errorMessage}</span>
     </div>
   </label>
 );
