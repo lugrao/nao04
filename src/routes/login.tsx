0357efe5562492a01c7c0c753808f6fcb92d0158
lugrao <elugrao@gmail.com>
2024-05-21 11:11:21 -0300

    adjust layout for mobile

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,11 +1,18 @@
+import { useWindowDimensions } from "src/utils/useWindowDimensions";
 import { LoginForm, CompanyLogo } from "src/components";
-export const Login = () => (
-  <div className="flex w-full flex-col lg:flex-row">
-    <div className="flex h-2/6 w-full items-center justify-center px-40 py-36 lg:h-full lg:w-6/12">
-      <CompanyLogo />
+
+export const Login = () => {
+  const { width } = useWindowDimensions();
+  return (
+    <div className="flex w-full flex-col items-center lg:flex-row">
+      <div className="flex h-min w-full max-w-[50rem] items-center justify-center px-24 pt-20 lg:h-full lg:w-6/12 lg:max-w-full">
+        {width && <CompanyLogo mobile={width < 1024} />}
+      </div>
+      <div className="flex h-full w-full items-center justify-center p-5 lg:w-6/12 lg:p-0">
+        <div className="flex h-min w-full max-w-[40rem] items-center justify-center rounded-2xl bg-white p-28 lg:h-full  lg:max-w-full lg:rounded-none">
+          <LoginForm />
+        </div>
+      </div>
     </div>
-    <div className="flex h-full w-full items-center justify-center bg-white lg:w-6/12">
-      <LoginForm />
-    </div>
-  </div>
-);
+  );
+};
