bc6953cb6fe8cdeec0e2742dbb8b97c0281117c3
lugrao <elugrao@gmail.com>
2024-05-21 11:02:10 -0300

    adjust height

--- src/components/authentication/FormButton.tsx
+++ src/components/authentication/FormButton.tsx
@@ -1,5 +1,5 @@
 export const FormButton = () => (
-  <button className="text-md btn w-full bg-indigo-900 font-medium text-white hover:bg-indigo-800">
+  <button className="text-md btn h-14 w-full bg-indigo-900 font-medium text-white hover:bg-indigo-800">
     Enviar
   </button>
 );
