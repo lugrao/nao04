0c112042b590cb873984a9eeb383230bd3782ca9
lugrao <elugrao@gmail.com>
2024-05-22 15:53:10 -0300

    vertically center form on desktop; fix scrolling issues

--- src/components/authentication/LoginForm.tsx
+++ src/components/authentication/LoginForm.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect, useState } from "react";
 import { FormInput } from "./FormInput";
 import { FormButton } from "./FormButton";
 import { Link } from "react-router-dom";
@@ -9,24 +10,44 @@ import { Link } from "react-router-dom";
  * @component
  * @returns {JSX.Element} The rendered LoginForm component.
  */
-export const LoginForm = (): JSX.Element => (
-  <div className="flex h-full w-full flex-col items-center gap-10 overflow-scroll bg-white lg:py-20">
-    <h1 className="mb-10 text-2xl font-bold">Iniciar sesión</h1>
-    <form
-      action=""
-      className="flex h-max w-full max-w-[50rem] flex-col items-center gap-4 lg:max-w-sm"
+export const LoginForm = (): JSX.Element => {
+  const formRef = useRef(null);
+  const [isOverflowing, setIsOverflowing] = useState(false);
+
+  useEffect(() => {
+    const checkOverflow = () => {
+      if (formRef.current) {
+        const { scrollHeight, clientHeight } = formRef.current;
+        setIsOverflowing(scrollHeight > clientHeight);
+      }
+    };
+    checkOverflow();
+    window.addEventListener("resize", checkOverflow);
+    return () => window.removeEventListener("resize", checkOverflow);
+  });
+
+  return (
+    <div
+      className={`flex h-full w-full flex-col items-center gap-10 bg-white p-28 ${!isOverflowing ? "justify-center" : ""}`}
+      ref={formRef}
     >
-      <FormInput labelText="Correo electrónico" errorMessage="Error" />
-      <FormInput labelText="Contraseña" errorMessage="Error" />
-      <div className="mt-8 w-full">
-        <FormButton />
-      </div>
-    </form>
-    <h2 className="mt-14">
-      ¿No eres usuario?{" "}
-      <Link className="font-semibold text-indigo-900" to="/signup">
-        Crea una cuenta.
-      </Link>
-    </h2>
-  </div>
-);
+      <h1 className="mb-10 text-2xl font-bold lg:mt-24">Iniciar sesión</h1>
+      <form
+        action=""
+        className="flex h-max w-full max-w-[50rem] flex-col items-center justify-around gap-4 lg:max-w-sm"
+      >
+        <FormInput labelText="Correo electrónico" errorMessage="Error" />
+        <FormInput labelText="Contraseña" errorMessage="Error" />
+        <div className="mt-8 w-full">
+          <FormButton />
+        </div>
+        <h2 className="mt-14 lg:mb-24">
+          ¿No eres usuario?{" "}
+          <Link className="font-semibold text-indigo-900" to="/signup">
+            Crea una cuenta.
+          </Link>
+        </h2>
+      </form>
+    </div>
+  );
+};
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -10,12 +10,12 @@ import { LoginForm, CompanyLogo } from "src/components";
 export const Login = (): JSX.Element => {
   const { width } = useWindowDimensions();
   return (
-    <div className="lg-max:overflow-x-scroll flex w-full flex-col items-center lg:flex-row">
-      <div className="flex h-min w-full max-w-[50rem] items-center justify-center px-24 pb-14 pt-20 lg:h-full lg:w-6/12 lg:max-w-full lg:p-0">
+    <div className="flex h-full w-full flex-col items-center lg:flex-row lg-max:overflow-y-auto">
+      <div className="flex h-min w-full max-w-[50rem] items-center justify-center px-24 pb-14 pt-20 lg:w-6/12 lg:max-w-full lg:p-0">
         {width && <CompanyLogo mobile={width < 1024} />}
       </div>
-      <div className="flex w-full grow items-center justify-center p-5 lg:h-full lg:w-6/12 lg:p-0">
-        <div className="flex h-min w-full max-w-[40rem] items-center justify-center rounded-2xl bg-white p-28 lg:h-full lg:max-w-full lg:rounded-none lg:p-0">
+      <div className="flex w-full grow items-center justify-center px-5 pb-20 pt-5 lg:h-full lg:w-6/12 lg:p-0">
+        <div className="flex h-min w-full max-w-[40rem] items-center justify-center overflow-y-auto   rounded-2xl bg-white lg:h-full lg:max-w-full  lg:rounded-none lg:p-0">
           <LoginForm />
         </div>
       </div>
