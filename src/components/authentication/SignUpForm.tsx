79998664a29d263db17433751592b1473cefbe80
lugrao <elugrao@gmail.com>
2024-05-22 15:37:46 -0300

    vertically center form on desktop; fix scrolling issues

--- src/components/authentication/SignUpForm.tsx
+++ src/components/authentication/SignUpForm.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect, useState } from "react";
 import { FormInput } from "./FormInput";
 import { FormButton } from "./FormButton";
 
@@ -7,21 +8,41 @@ import { FormButton } from "./FormButton";
  * @component
  * @returns {JSX.Element} The rendered SignUpForm component.
  */
-export const SignUpForm = (): JSX.Element => (
-  <div className="flex h-full w-full flex-col items-center gap-10 overflow-scroll bg-white lg:py-20">
-    <h1 className="mb-10 text-2xl font-bold">Registrarse</h1>
-    <form
-      action=""
-      className="flex h-max w-full max-w-[50rem] flex-col items-center gap-4 lg:max-w-sm"
+export const SignUpForm = (): JSX.Element => {
+  const formRef = useRef(null);
+  const [isOverflowing, setIsOverflowing] = useState(false);
+
+  useEffect(() => {
+    const checkOverflow = () => {
+      if (formRef.current) {
+        const { scrollHeight, clientHeight } = formRef.current;
+        setIsOverflowing(scrollHeight > clientHeight);
+      }
+    };
+    checkOverflow();
+    window.addEventListener("resize", checkOverflow);
+    return () => window.removeEventListener("resize", checkOverflow);
+  });
+
+  return (
+    <div
+      className={`flex h-full w-full flex-col items-center gap-10 bg-white p-28 ${!isOverflowing ? "justify-center" : ""}`}
+      ref={formRef}
     >
-      <FormInput labelText="Nombre" errorMessage="Error" />
-      <FormInput labelText="Apellido" errorMessage="Error" />
-      <FormInput labelText="Correo electrónico" errorMessage="Error" />
-      <FormInput labelText="Contraseña" errorMessage="Error" />
-      <FormInput labelText="Repetir contraseña" errorMessage="Error" />
-      <div className="mt-8 w-full">
-        <FormButton />
-      </div>
-    </form>
-  </div>
-);
+      <h1 className="mb-10 text-2xl font-bold lg:mt-24">Registrarse</h1>
+      <form
+        action=""
+        className="flex h-max w-full max-w-[50rem] flex-col items-center justify-around gap-4 lg:max-w-sm"
+      >
+        <FormInput labelText="Nombre" errorMessage="Error" />
+        <FormInput labelText="Apellido" errorMessage="Error" />
+        <FormInput labelText="Correo electrónico" errorMessage="Error" />
+        <FormInput labelText="Contraseña" errorMessage="Error" />
+        <FormInput labelText="Repetir contraseña" errorMessage="Error" />
+        <div className="mt-8 w-full lg:mb-24">
+          <FormButton />
+        </div>
+      </form>
+    </div>
+  );
+};
