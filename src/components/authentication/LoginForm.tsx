57b64dfc31a919928268eafc50160b84496ca8e8
lugrao <elugrao@gmail.com>
2024-05-23 13:11:39 -0300

    add form validation with formik and yup

--- src/components/authentication/LoginForm.tsx
+++ src/components/authentication/LoginForm.tsx
@@ -2,6 +2,26 @@ import { useRef, useEffect, useState } from "react";
 import { FormInput } from "./FormInput";
 import { FormButton } from "./FormButton";
 import { Link } from "react-router-dom";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+
+/**
+ * Validation schema for the login form using Yup.
+ *
+ * Fields:
+ * - `email`: Must be a valid email address and is required.
+ * - `password`: Must be at least 8 characters long, contain at least one
+ * number, one lowercase letter, and one uppercase letter. Is required.
+ */
+const LoginSchema = Yup.object().shape({
+  email: Yup.string().email("Correo no válido").required("Requerido"),
+  password: Yup.string()
+    .min(8, "La contraseña debe ser de 8 caracteres o más")
+    .matches(/[0-9]/, "La contraseña requiere un número")
+    .matches(/[a-z]/, "La contraseña requiere una letra minúscula")
+    .matches(/[A-Z]/, "La contraseña requiere una letra mayúscula")
+    .required("Requerido"),
+});
 
 /**
  * Renders a login form with input fields for email and password, a submit
@@ -44,22 +64,43 @@ export const LoginForm = (): JSX.Element => {
       ref={formRef}
     >
       <h1 className="mb-10 text-2xl font-bold lg:mt-24">Iniciar sesión</h1>
-      <form
-        action=""
-        className="flex h-max w-full max-w-[50rem] flex-col items-center justify-around gap-4 lg:max-w-sm"
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        validationSchema={LoginSchema}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
       >
-        <FormInput labelText="Correo electrónico" errorMessage="Error" />
-        <FormInput labelText="Contraseña" errorMessage="Error" />
-        <div className="mt-8 w-full">
-          <FormButton />
-        </div>
-        <h2 className="mt-14 lg:mb-24">
-          ¿No eres usuario?{" "}
-          <Link className="font-semibold text-indigo-900" to="/signup">
-            Crea una cuenta.
-          </Link>
-        </h2>
-      </form>
+        <Form
+          action=""
+          className="flex h-max w-full max-w-[50rem] flex-col items-center justify-around gap-4 lg:max-w-sm"
+        >
+          <FormInput
+            label="Correo electrónico"
+            id="input-email"
+            name="email"
+            type="text"
+          />
+          <FormInput
+            label="Contraseña"
+            id="input-password"
+            name="password"
+            type="password"
+          />
+          <div className="mt-8 w-full">
+            <FormButton />
+          </div>
+          <h2 className="mt-14 lg:mb-24">
+            ¿No eres usuario?{" "}
+            <Link className="font-semibold text-indigo-900" to="/signup">
+              Crea una cuenta.
+            </Link>
+          </h2>
+        </Form>
+      </Formik>
     </div>
   );
 };
--- src/components/authentication/SignUpForm.tsx
+++ src/components/authentication/SignUpForm.tsx
@@ -1,6 +1,38 @@
 import { useRef, useEffect, useState } from "react";
 import { FormInput } from "./FormInput";
 import { FormButton } from "./FormButton";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+
+/**
+ * Validation schema for the sign-up form using Yup.
+ *
+ * Fields:
+ * - `firstName`: Must be at least 1 character long and is required.
+ * - `lastName`: Must be at least 1 character long and is required.
+ * - `email`: Must be a valid email address and is required.
+ * - `password`: Must be at least 8 characters long, contain at least one
+ * number, one lowercase letter, and one uppercase letter. Is required.
+ * - `passwordConfirm`: Must match the `password` field. Is required.
+ */
+const SignUpSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(1, "El nombre debe ser de 1 caracter o más")
+    .required("Requerido"),
+  lastName: Yup.string()
+    .min(1, "El apellido debe ser de 1 caracter o más")
+    .required("Requerido"),
+  email: Yup.string().email("Correo no válido").required("Requerido"),
+  password: Yup.string()
+    .min(8, "La contraseña debe ser de 8 caracteres o más")
+    .matches(/[0-9]/, "La contraseña requiere un número")
+    .matches(/[a-z]/, "La contraseña requiere una letra minúscula")
+    .matches(/[A-Z]/, "La contraseña requiere una letra mayúscula")
+    .required("Requerido"),
+  passwordConfirm: Yup.string()
+    .oneOf([Yup.ref("password")], "Las contraseñas no coinciden")
+    .required("Requerido"),
+});
 
 /**
  * Renders a sign-up form with multiple input fields and a submit button.
@@ -42,19 +74,58 @@ export const SignUpForm = (): JSX.Element => {
       ref={formRef}
     >
       <h1 className="mb-10 text-2xl font-bold lg:mt-24">Registrarse</h1>
-      <form
-        action=""
-        className="flex h-max w-full max-w-[50rem] flex-col items-center justify-around gap-4 lg:max-w-sm"
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+          password: "",
+          passwordConfirm: "",
+        }}
+        validationSchema={SignUpSchema}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
       >
-        <FormInput labelText="Nombre" errorMessage="Error" />
-        <FormInput labelText="Apellido" errorMessage="Error" />
-        <FormInput labelText="Correo electrónico" errorMessage="Error" />
-        <FormInput labelText="Contraseña" errorMessage="Error" />
-        <FormInput labelText="Repetir contraseña" errorMessage="Error" />
-        <div className="mt-8 w-full lg:mb-24">
-          <FormButton />
-        </div>
-      </form>
+        <Form
+          action=""
+          className="flex h-max w-full max-w-[50rem] flex-col items-center justify-around gap-4 lg:max-w-sm"
+        >
+          <FormInput
+            label="Nombre"
+            id="input-first-name"
+            name="firstName"
+            type="text"
+          />
+          <FormInput
+            label="Apellido"
+            id="input-last-name"
+            name="lastName"
+            type="text"
+          />
+          <FormInput
+            label="Correo electrónico"
+            id="input-email"
+            name="email"
+            type="email"
+          />
+          <FormInput
+            label="Contraseña"
+            id="input-password"
+            name="password"
+            type="password"
+          />
+          <FormInput
+            label="Repetir contraseña"
+            id="input-password-confirm"
+            name="passwordConfirm"
+            type="password"
+          />
+          <div className="mt-8 w-full lg:mb-24">
+            <FormButton />
+          </div>
+        </Form>
+      </Formik>
     </div>
   );
 };
