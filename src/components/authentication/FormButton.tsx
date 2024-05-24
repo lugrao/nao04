6a96de7253601edba11c98444ede95708682a7dc
lugrao <elugrao@gmail.com>
2024-05-24 11:29:27 -0300

    add type attribute to button

--- src/components/authentication/FormButton.tsx
+++ src/components/authentication/FormButton.tsx
@@ -5,7 +5,10 @@
  * @returns {JSX.Element} The rendered FormButton component.
  */
 export const FormButton = (): JSX.Element => (
-  <button className="text-md btn h-14 w-full bg-indigo-900 font-medium text-white hover:bg-indigo-800">
+  <button
+    type="submit"
+    className="text-md btn h-14 w-full bg-indigo-900 font-medium text-white hover:bg-indigo-800"
+  >
     Enviar
   </button>
 );
