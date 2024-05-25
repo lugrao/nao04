9f5bb4934d26ad3cd02e43f5e5f6fe051d219629
lugrao <elugrao@gmail.com>
2024-05-25 16:27:28 -0300

    configure routes

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,11 +1,18 @@
 import "routes/root.css";
+import { useEffect, useState } from "react";
 import { useAppSelector, useAppDispatch } from "src/redux/hooks";
 import {
   selectActiveSession,
   updateActiveSession,
 } from "src/components/authentication/userSlice";
-import { Outlet, useNavigate, useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Login } from "./login";
+import { SignUp } from "./signup";
+import { Dashboard } from "./dashboard";
+import { Home } from "src/components";
+import { Sales } from "src/components";
+import { Products } from "src/components";
+import { Invoices } from "src/components";
 
 /**
  * Renders the root component which serves as the main layout container.
@@ -16,8 +23,6 @@ import { useEffect, useState } from "react";
 export const Root = (): JSX.Element => {
   const dispatch = useAppDispatch();
   const activeSession = useAppSelector(selectActiveSession);
-  const navigate = useNavigate();
-  const { pathname } = useLocation();
   const [isLoading, setIsLoading] = useState(true);
 
   /**
@@ -29,47 +34,66 @@ export const Root = (): JSX.Element => {
     if (!activeSession) {
       dispatch(updateActiveSession());
     }
-  }, []);
-
-  useEffect(() => {
-    switch (pathname) {
-      case "/":
-        if (activeSession) {
-          navigate("/dashboard");
-        } else {
-          navigate("/login");
-        }
-        break;
-      case "/login":
-        if (activeSession) {
-          navigate("/dashboard");
-        } else {
-          navigate("/login");
-        }
-        break;
-      case "/signup":
-        if (activeSession) {
-          navigate("/dashboard");
-        } else {
-          navigate("/signup");
-        }
-        break;
-      default:
-        if (activeSession) {
-          navigate(pathname);
-        } else {
-          navigate("/login");
-        }
-        break;
-    }
     setIsLoading(false);
-  }, [activeSession]);
+  }, []);
 
   return (
     <div className="flex h-full w-full items-center justify-center p-2">
       <div className="flex h-full w-full rounded-3xl bg-white p-0.5">
         <div className="flex h-full w-full overflow-hidden rounded-3xl bg-violet-50">
-          {!isLoading && <Outlet />}
+          {!isLoading && (
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  activeSession ? (
+                    <Navigate replace to={"/dashboard"} />
+                  ) : (
+                    <Navigate replace to={"/login"} />
+                  )
+                }
+              />
+              <Route
+                path="/login"
+                element={
+                  activeSession ? (
+                    <Navigate replace to={"/dashboard"} />
+                  ) : (
+                    <Login />
+                  )
+                }
+              />
+              <Route
+                path="/signup"
+                element={
+                  activeSession ? (
+                    <Navigate replace to={"/dashboard"} />
+                  ) : (
+                    <SignUp />
+                  )
+                }
+              />
+              <Route
+                path="/dashboard"
+                element={
+                  activeSession ? (
+                    <Dashboard />
+                  ) : (
+                    <Navigate replace to={"/login"} />
+                  )
+                }
+              >
+                <Route path="/dashboard" element={<Home />} />
+                <Route path="/dashboard/sales" element={<Sales />} />
+                <Route path="/dashboard/products" element={<Products />} />
+                <Route path="/dashboard/invoices" element={<Invoices />} />
+                <Route
+                  path="/dashboard/logout"
+                  element={<Navigate replace to={"/"} />}
+                />
+              </Route>
+            </Routes>
+          )}
         </div>
       </div>
     </div>
