46e57236dc643b5bd02d78b40d1b6da3d20a2e49
lugrao <elugrao@gmail.com>
2024-05-25 16:26:35 -0300

    remove custom router

--- src/main.tsx
+++ src/main.tsx
@@ -1,62 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { Root } from "routes/root.js";
-import { Dashboard } from "./routes/dashboard";
-import { Home } from "./components";
-import { Sales } from "./components";
-import { Products } from "./components";
-import { Invoices } from "./components";
 import { store } from "./redux/store";
-import { Login } from "./routes/login";
-import { SignUp } from "./routes/signup";
 import "src/index.css";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <div>Error.</div>,
-    children: [
-      {
-        path: "dashboard",
-        element: <Dashboard />,
-        children: [
-          {
-            path: "",
-            element: <Home />,
-          },
-          {
-            path: "sales",
-            element: <Sales />,
-          },
-          {
-            path: "products",
-            element: <Products />,
-          },
-          {
-            path: "invoices",
-            element: <Invoices />,
-          },
-        ],
-      },
-      {
-        path: "login",
-        element: <Login />,
-      },
-      {
-        path: "signup",
-        element: <SignUp />,
-      },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <Root />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 );
