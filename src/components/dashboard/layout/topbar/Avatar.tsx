0281480b3476c9dd0a00131eb5532c29978037d4
lugrao <elugrao@gmail.com>
2024-05-28 13:18:51 -0300

    change cursor on mouse hover

--- src/components/dashboard/layout/topbar/Avatar.tsx
+++ src/components/dashboard/layout/topbar/Avatar.tsx
@@ -5,7 +5,7 @@
  * @returns {JSX.Element} JSX element representing the Avatar component.
  */
 export const Avatar = (): JSX.Element => (
-  <div className="h-min min-w-16 p-2">
+  <div className="h-min min-w-16 cursor-pointer p-2">
     <div className="flex justify-center overflow-hidden rounded-full bg-white">
       <img width={50} height={50} src="/user.svg" alt="Ícono de usuario" />
     </div>
--- src/components/dashboard/layout/topbar/Notifications.tsx
+++ src/components/dashboard/layout/topbar/Notifications.tsx
@@ -6,7 +6,7 @@
  */
 export const Notifications = (): JSX.Element => (
   <div className="flex h-min gap-12 p-2">
-    <div className="w-7">
+    <div className="w-7 cursor-pointer">
       <img
         width={26}
         height={26}
@@ -14,7 +14,7 @@ export const Notifications = (): JSX.Element => (
         alt="Ícono de notificación de mensajes"
       />
     </div>
-    <div className="w-7">
+    <div className="w-7 cursor-pointer">
       <img
         width={24}
         height={24}
