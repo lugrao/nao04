5b7c6a07e6b6230c724bdd8bf29c3996d6bdae41
lugrao <elugrao@gmail.com>
2024-05-29 11:33:09 -0300

    add test config object

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import { resolve } from "path";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./tests/setup.ts",
+  },
   resolve: {
     alias: {
       src: resolve(__dirname, "src"),
