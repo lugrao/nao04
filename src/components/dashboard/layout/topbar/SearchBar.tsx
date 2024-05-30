f9e37ca4160ccbb926fe3a19f0269cd458b1951a
lugrao <elugrao@gmail.com>
2024-05-30 12:02:14 -0300

    add label for screen readers

--- src/components/dashboard/layout/topbar/SearchBar.tsx
+++ src/components/dashboard/layout/topbar/SearchBar.tsx
@@ -6,11 +6,19 @@
  */
 export const SearchBar = (): JSX.Element => (
   <div className="h-max p-2">
+    <label htmlFor="input-search" className="sr-only">
+      Buscar
+    </label>
     <label className="input flex h-9 items-center gap-2">
       <div className="h-4 w-4">
         <img width={17} height={17} src="/search.svg" alt="Ícono de búsqueda" />
       </div>
-      <input type="text" className="grow" placeholder="Buscar" />
+      <input
+        id="input-search"
+        type="text"
+        className="grow"
+        placeholder="Buscar"
+      />
     </label>
   </div>
 );
