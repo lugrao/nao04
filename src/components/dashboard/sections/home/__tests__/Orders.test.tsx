fbb48538f28b247aa2d6858bc6d2b23ba8829331
lugrao <elugrao@gmail.com>
2024-05-30 18:03:25 -0300

    add tests for the background image and the number of orders

--- src/components/dashboard/sections/home/__tests__/Orders.test.tsx
+++ src/components/dashboard/sections/home/__tests__/Orders.test.tsx
@@ -2,11 +2,25 @@ import { render, screen } from "@testing-library/react";
 import { Orders } from "../Orders";
 
 describe("Orders", () => {
-    render(<Orders amount={123} status="new"/>)
-    
-    const ordersNew = screen.getByRole('heading', {name:/pedidos nuevos/i})
-    
-    it('renders the Orders component', ()=>{
-        expect(ordersNew).toBeTruthy()
-    })
+  const ordersAmount = 123;
+
+  it("renders a title heading", () => {
+    render(<Orders amount={ordersAmount} status="new" />);
+    const ordersNew = screen.getByRole("heading", { name: /pedidos nuevos/i });
+    expect(ordersNew).toBeTruthy();
+  });
+
+  it("renders the background image", () => {
+    render(<Orders amount={ordersAmount} status="new" />);
+    const image = screen.getByAltText(/bolsa de compras/i);
+    expect(image).toBeTruthy();
+  });
+
+  it("renders the number of orders", () => {
+    render(<Orders amount={ordersAmount} status="new" />);
+    const numberOfOrders = screen.getByRole("heading", {
+      name: ordersAmount.toString(),
+    });
+    expect(numberOfOrders).toBeTruthy();
+  });
 });
