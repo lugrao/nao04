11c3486f07f7bc4f4a8463d7283601e8c348a9c8
lugrao <elugrao@gmail.com>
2024-05-31 13:37:14 -0300

    add test for data

--- src/components/dashboard/sections/home/__tests__/BestSellingProductsTable.test.tsx
+++ src/components/dashboard/sections/home/__tests__/BestSellingProductsTable.test.tsx
@@ -21,4 +21,21 @@ describe("BestSellingProductsTable", () => {
     expect(cells[0].textContent).toBe(data[0].productName);
     expect(cells[1].textContent).toBe(data[0].salesPercentage.toString() + "%");
   });
+
+  it("renders the data correctly", () => {
+    render(<BestSellingProductsTable data={data} />);
+    const table = screen.getByRole("table");
+    const rows = within(table).getAllByRole("row");
+    const firstRowCells = within(rows[1]).getAllByRole("cell");
+    const secondRowCells = within(rows[2]).getAllByRole("cell");
+
+    expect(firstRowCells[0].textContent).toBe(data[0].productName);
+    expect(firstRowCells[1].textContent).toBe(
+      data[0].salesPercentage.toString() + "%",
+    );
+    expect(secondRowCells[0].textContent).toBe(data[1].productName);
+    expect(secondRowCells[1].textContent).toBe(
+      data[1].salesPercentage.toString() + "%",
+    );
+  });
 });
