e60eb2a773a4e706fd8eb385dc206985f08b93e2
lugrao <elugrao@gmail.com>
2024-06-03 14:22:41 -0300

    add tests for the login functionality, and for error messages

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,66 @@
+/// <reference types="cypress" />
+
+describe("Login page", () => {
+  const user = {
+    firstName: "Jerry",
+    lastName: "Seinfeld",
+    email: "seinfeld@mail.com",
+    password: "asdQWE123",
+  };
+
+  before(() => {
+    cy.window().then((window) => {
+      window.localStorage.setItem("seinfeld@mail.com", JSON.stringify(user));
+    });
+  });
+
+  it("lets the user log in", () => {
+    cy.visit("http://localhost:5173/login");
+
+    cy.get("#input-email").should("be.visible").type(user.email);
+    cy.get("#input-password").should("be.visible").type(user.password);
+    cy.get("button").should("be.visible").click();
+  });
+});
+
+describe("Error messages", () => {
+  it("displays error messages for invalid inputs", () => {
+    cy.visit("http://localhost:5173/login");
+
+    // required field
+    cy.get("#input-email").should("be.visible").click();
+    cy.get("#input-password").should("be.visible").click();
+    cy.get("#input-email-error")
+      .should("be.visible")
+      .should("include.text", "Requerido");
+
+    // password too short
+    cy.get("#input-password").should("be.visible").type("tooshor");
+    cy.get("#input-email").should("be.visible").click();
+    cy.get("#input-password-error")
+      .should("be.visible")
+      .should("include.text", "La contraseña debe ser de 8 caracteres o más");
+
+    // password must have a number
+    cy.get("#input-password")
+      .should("be.visible")
+      .clear()
+      .type("not a number in sight");
+    cy.get("#input-email").should("be.visible").click();
+    cy.get("#input-password-error")
+      .should("be.visible")
+      .should("include.text", "La contraseña requiere un número");
+
+    // password must have a capital letter
+    cy.get("#input-password")
+      .should("be.visible")
+      .clear()
+      .type(
+        "this is a password with 2 numbers and more than 8 characters, but with no capital letter. can you believe it?",
+      );
+    cy.get("#input-email").should("be.visible").click();
+    cy.get("#input-password-error")
+      .should("be.visible")
+      .should("include.text", "La contraseña requiere una letra mayúscula");
+  });
+});
